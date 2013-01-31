de5b3eef90a62a482cbd90d6bbed3e33dc50d3f9
James Wade <jpswade@gmail.com>
2013-01-31 13:28:30 +0000

    Almost a complete rewrite into something more functional that resembles the real smushit api.

--- ws.php
+++ ws.php
@@ -1,76 +1,76 @@
 <?php
 
-/* Required */
-require('Smushit.lib.php');
+/* Settings */
+$prefix = 'smush-';
+$uploadpath = 'upload';
+$resultspath = 'results';
+
+/* Inputs */
+$img = isset($_GET['img']) ? urldecode($_GET['img']) : null;
 
 /* Variables */
 $response = array();
-$img = urldecode($_GET['img']);
-$fileInfo = $_FILES['files'];
-$prefix = 'smush-';
-$uploadpath = 'upload';
-$downloadpath = 'download';
+$res = false;
+$uploadpath = realpath($uploadpath) . DIRECTORY_SEPARATOR;
+$resultspath = realpath($resultspath) . DIRECTORY_SEPARATOR;
+
+/* Preflight Checks */
+if (!is_dir($uploadpath)) {
+    user_error('Upload path is invalid');
+    die();
+}
+if (!is_writeable($uploadpath)) {
+    user_error('Upload path is NOT writable');
+    die();
+}
+if (!is_dir($resultspath)) {
+    user_error('Results path is invalid');
+    die();
+}
+if (!is_writeable($resultspath)) {
+    user_error('Results path is NOT writable');
+    die();
+}
+
+/* Required */
+require('Smushit.lib.php');
 
 /* Logic */
-if (!$img && ($fileInfo == NULL || $fileInfo['error'] != NULL)) {
-    $response['error'] = 'Need an ?img=';
+if ($img) {
+    //web upload
+    //check for multiple URLS?
+    $matches = array();
+    preg_match('/^https?:\/\/.+/i', $img, $matches);
+    $match = array_shift($matches);
+    $filename = basename($match);
+    if (!$filename) {
+        $filename = 'image';
+    }
+    if (!strrchr($filename, '.')) {
+        $filename .= '.png';
+    }
+    $file = uniqid($prefix) . '-' . $filename;
+    $srcfile = $uploadpath . $file;
+    $res = copy($match, $srcfile);
+} elseif (isset($_FILES)) {
+    $fileInfo = array_shift($_FILES);
+    $fileType = $fileInfo['type'];
+    $fileTemp = $fileInfo['tmp_name'];
+    $file = uniqid($prefix) . '-' . $fileInfo['name'];
+    $srcfile = $uploadpath . $file;
+    $res = move_uploaded_file($fileTemp, $srcfile);
 } else {
-    $smushit = new Smushit();
-
-    if ($img) {
-        $matches = array();
-
-        if (preg_match('/^https?:\/\/.+/i', $img, $matches)) {
-
-            $filename = preg_replace('/^http.+\//i', '', $img);
-            $filename = preg_replace('/\?.+/', '', $filename);
-            $filename = preg_replace('/\#.+/', '', $filename);
-
-            if ($filename == '') {
-                $filename = 'image';
-            }
-            $ext = strrchr($filename, '.');
-            if ($ext == '') {
-                $filename = $filename . '.png';
-            }
-
-            $file = uniqid($prefix) . '-' . $filename;
-            $filepath = $uploadpath . DIRECTORY_SEPARATOR . $file;
-            $res = $smushit->copy($img, $filepath);
-        } else {
-            $response['code'] = 400;
-            $response['error'] = 'img paramter is invalid';
-            echo json_encode($response);
-            return;
-        }
-    } else {
-        $fileType = $fileInfo['type'];
-        $fileTemp = $fileInfo['tmp_name'];
-        $file = uniqid($prefix) . '-' . $fileInfo['name'];
-        $filepath = $uploadpath . DIRECTORY_SEPARATOR . $file;
-        $res = move_uploaded_file($fileTemp, $filepath);
-    }
-
-    if (!$res) {
-        $response['code'] = 500;
-        $response['error'] = 'error occur while save file';
-    } else {
-
-        $path = $downloadpath . DIRECTORY_SEPARATOR;
-        $response = $smushit->optimize($filepath, $path . $file);
-
-        if ($response['error'] != NULL) {
-            $response['code'] = 500;
-        } else {
-            if ($response['src_size'] == $response['dest_size']) {
-                $response['error'] = 'No savings.';
-                $response['src_size'] = -1;
-            }
-            $response['code'] = 200;
-        }
-    }
+    //no file given
+    $response['error'] = 'Need an ?img=';
 }
-$response['id'] = isset($id) ? $id : null;
+
+if ($res) {
+    $destfile = $resultspath . $file;
+    $smushit = new Smushit();
+    $response = $smushit->optimize($srcfile, $destfile);
+    $response['id'] = isset($_REQUEST['id']) ? urlencode($_REQUEST['id']) : '';
+    //print_r($smushit->dbg);
+}
 header('Content-Type:application/json');
-echo json_encode($response);
+echo json_encode($response ? $response : null);
 //eof
